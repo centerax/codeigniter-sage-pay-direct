90ad5b8de894a79b7896d9bed3bda23ddef88055
Centerax <pablos.benitez@gmail.com>
2011-07-10 21:21:24 -0300

    direct integration without 3d

--- libraries/Sagepay_direct.php
+++ libraries/Sagepay_direct.php
@@ -1,10 +1,41 @@
-<?php
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * CodeIgniter SagePay DIRECT integration library
+ *
+ * Perform transactions using http://sagepay.com/ DIRECT protocol
+ *
+ * @package        	CodeIgniter
+ * @subpackage    	Libraries
+ * @category    	Libraries
+ * @author        	Pablo S. Benitez
+ * @license         http://www.opensource.org/licenses/mit-license.html
+ * @link			http://getsparks.org/packages/<TODO>/show
+ */
 class Sagepay_direct
 {
+	/**
+	 * Code Igniter instance
+	 * @var object
+	 */
 	private $_ci;
+
+	/**
+	 * Debug flag, you can $this->sagepay_direct->debug = true; or $this->sagepay_direct->set_options(array('debug'=>true));
+	 * @var boolean
+	 */
 	public $debug = false;
+
+	/**
+	 * Store configuration data
+	 * @var array
+	 */
 	private $_config = array();
+
+	/**
+	 * SagePay NOT valid response codes
+	 * @var array
+	 */
 	protected $_invalid_response_statuses = array(
 													'ERROR',
 													'INVALID',
@@ -32,6 +63,11 @@ class Sagepay_direct
 		$this->_ci->load->helper('inflector');
 	}
 
+	/**
+	 * Options setter
+	 * @param array $options
+	 * @return void
+	 */
     public function set_options($options = array())
     {
 		if( count($options) ){
@@ -44,6 +80,14 @@ class Sagepay_direct
 		}
     }
 
+	/**
+	 * Place (register) a transaction to SagePay
+	 * @param float $amount Amount to be charged
+	 * @param array $cc Credit Card information
+	 * @param array $billing Billing information
+	 * @param array $shipping OPTIONAL, if not sent $billing will be copied
+	 * @param array $additional Here you can add or override information such as Currency, CustomerEMail, Description, etc.
+	 */
 	public function place($amount, $cc, $billing, $shipping = null, $additional = array())
 	{
 		// We have to send delivery data to SagePay so if no shipping data is
@@ -52,6 +96,7 @@ class Sagepay_direct
 			$shipping = $this->billing_as_shipping($billing);
 		}
 
+		// Initialize post array
 		$post = array();
 
 		$post ['VPSProtocol'] = $this->_config['protocol_version'];
@@ -61,17 +106,23 @@ class Sagepay_direct
 		$post ['Amount'] = number_format($amount, 2); //Formatted to 2 decimal places with leading digit but no commas or currency symbols
 		$post ['Currency'] = $this->_config['currency'];
 
+		// Add customer's IP address only if it is valid
 		if( $this->_ci->input->valid_ip($this->_ci->input->ip_address()) ){
 			$post ['ClientIPAddress'] = $this->_ci->input->ip_address();
 		}
 
-		$post ['Apply3DSecure'] = 2;
+		/*// 3D checks flag
+		$post ['Apply3DSecure'] = 2;*/
+
+		// Default is 'E' for Ecommerce
 		$post ['AccountType'] = $this->_config['account_type'];
 
+		// Adding CreditCard, Billing and Delivery information
 		$post += $this->to_camel_keys($cc);
 		$post += $this->to_camel_keys($billing);
 		$post += $this->to_camel_keys($shipping);
 
+		// If no description is available, send a dummy one because it's required
 		if(!isset($post['Description'])){
 			$post['Description'] = 'Please enter description to be sent to Sage Pay.';
 		}
@@ -83,15 +134,25 @@ class Sagepay_direct
 			array_merge($post, $additional);
 		}
 
+
 		return $this->_post_request($post);
 	}
 
+	/**
+	 * Return purchase URL for current mode
+	 * @return string The purchase url
+	 */
 	protected function _get_post_url()
 	{
 		return $this->_config[ $this->_config['mode'] . '_purchase_url' ];
 	}
 
-	protected function _post_request($data)
+	/**
+	 * POST request wrapper
+	 * @param array $data Parameters to be posted
+	 * @return array Response information
+	 */
+	protected function _post_request(array $data)
 	{
 		// Initialize cURL session
         $this->_ci->curl->create($this->_get_post_url());
@@ -162,11 +223,20 @@ class Sagepay_direct
     	return ucfirst( camelize($str) );
 	}
 
+	/**
+	 * Generate an unique vendor tx code formatted string
+	 * @return string Vendor Tx Code (max 40 chars)
+	 */
 	protected function _get_vendor_tx_code()
 	{
 		return $this->clean_input( $this->ls((date('d-m-Y_H.i.s') . '_' . random_string('alnum', 10)), 40), 'VendorTxCode' );
 	}
 
+	/**
+	 * Copy billing information to delivery
+	 * @param array Billing data
+	 * @return array Shipping data
+	 */
 	public function billing_as_shipping(array $billing_data)
 	{
 		$newkeys = array();
@@ -177,13 +247,25 @@ class Sagepay_direct
 		return array_combine($newkeys, array_values($billing_data));
 	}
 
+	/**
+	 * Cut string to certain $length, will start from position zero
+	 * @param string $str String to cut
+	 * @param int $length Length
+	 * @return string Shorter string
+	 */
 	public function ls($str, $length)
 	{
 		return substr($str, 0, $length);
 	}
 
-	// Filters unwanted characters out of an input string.  Useful for tidying up FORM field inputs
-	function clean_input($strRawText, $strType = '')
+	/**
+	 * From SagePay PHP-Integration-Kit.
+	 * Filters unwanted characters out of an input string.
+	 * @param string $strRawText Text to filter
+	 * @param string $strType OPTIONAL, Type of string
+	 * @return string Filtered string
+	 */
+	public function clean_input($strRawText, $strType = '')
 	{
 
 		if ($strType=="Number") {
